42a020139a23138211276ae1944c9886ae00cb9a
moriaty <19520624@gm.uit.edu.vn>
2020-11-26 23:21:46 +0700

    Move pre_proc into 1 function

--- pre_proc.py
+++ pre_proc.py
@@ -3,36 +3,42 @@ from PIL import Image
 
 INP_DIR = '/dataset/test_set_A_full'
 
-INP_DIR = INP_DIR + '/'
-NONE_DIR = os.path.dirname(INP_DIR) + '_none'
-SQUARE_DIR = os.path.dirname(INP_DIR) + '_square'
-CROP_DIR = os.path.dirname(INP_DIR) + '_cropped'
 
-os.makedirs(NONE_DIR, exist_ok=True)
-os.makedirs(SQUARE_DIR, exist_ok=True)
-os.makedirs(CROP_DIR, exist_ok=True)
+# Lọc thư mục data test ra thành 3 thư mục: None, Square (1:1), và phần còn lại (đã được crop ngay chính giữa)
+def pre_proc(INP_DIR):
+    INP_DIR = INP_DIR + '/'
+    NONE_DIR = os.path.dirname(INP_DIR) + '_none'
+    SQUARE_DIR = os.path.dirname(INP_DIR) + '_square'
+    CROP_DIR = os.path.dirname(INP_DIR) + '_cropped'
 
-dir = os.listdir(INP_DIR)
-dir.sort()
+    os.makedirs(NONE_DIR, exist_ok=True)
+    os.makedirs(SQUARE_DIR, exist_ok=True)
+    os.makedirs(CROP_DIR, exist_ok=True)
 
-for fi in dir:
-    print(fi)
-    inp_path = os.path.join(INP_DIR, fi)
-    
-    img = Image.read(inp_path)
-    if img.format == 'GIF': 
-        shutil.move(inp_path, os.path.join(NONE_DIR, fi))
-        continue
-    width, height = img.size
-    if width == height:
-        shutil.copyfile(inp_path, os.path.join(SQUARE_DIR, fi))
-        continue
-    
-    img = cv2.imread(inp_path)
-    if width > height:
-        img = img[:, width//2-height//2:width//2+height//2]
-    else:
-        img = img[height//2-width//2:height//2+width//2, :]
+    dir = os.listdir(INP_DIR)
+    dir.sort()
+
+    for fi in dir:
+        print(fi)
+        inp_path = os.path.join(INP_DIR, fi)
         
-    cv2.imwrite(os.path.join(CROP_DIR, fi), img)
+        img = Image.read(inp_path)
+        if img.format == 'GIF': 
+            shutil.move(inp_path, os.path.join(NONE_DIR, fi))
+            continue
+        width, height = img.size
+        if width == height:
+            shutil.copyfile(inp_path, os.path.join(SQUARE_DIR, fi))
+            continue
+        
+        img = cv2.imread(inp_path)
+        if width > height:
+            img = img[:, width//2-height//2:width//2+height//2]
+        else:
+            img = img[height//2-width//2:height//2+width//2, :]
+            
+        cv2.imwrite(os.path.join(CROP_DIR, fi), img)
+        
+if __name__ == '__main__':
+    pre_proc(INP_DIR)
         
