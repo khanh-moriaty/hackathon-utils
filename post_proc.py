d0641c9cc84515feb61c1e9407dbca34d90da03a
moriaty <19520624@gm.uit.edu.vn>
2020-11-26 23:17:48 +0700

    Move post_proc into 1 function

--- post_proc.py
+++ post_proc.py
@@ -2,25 +2,13 @@ import os
 import shutil
 from PIL import Image
 
-INP_SUB_FILE = 'team007_b8tf_datacrop_5epoch.txt'
 INP_SUB_PATH = '/storage/timm/'
+INP_SUB_FILE = 'team007_b8tf_datacrop_5epoch.txt'
 
-OUT_SUB_FILE = 'team007_b8tf_datacrop_5epoch_threshold0.txt'
 OUT_SUB_PATH = '/storage/submissions/'
-fn, _ = os.path.splitext(OUT_SUB_FILE)
+OUT_SUB_FILE = 'team007_b8tf_datacrop_5epoch_threshold0.txt'
 
-TEST_SET_DIR = '/dataset/test_set_A_full'
-test_dir = os.path.dirname(TEST_SET_DIR+'/')
-
-CROP_DIR = test_dir + '_cropped'
-NONE_DIR = test_dir + '_none'
-SQUARE_DIR = test_dir + '_square'
-
-with open(os.path.join(INP_SUB_PATH, INP_SUB_FILE), 'r') as f:
-    lines = f.read().splitlines()
-
-lines = [line.strip().split() for line in lines]
-lines.sort(key=lambda x: x[0])
+TEST_SET_DIR = '/dataset/test_set_A_full/'
 
 THRESHOLD = {
     '0': 0,
@@ -32,57 +20,75 @@ THRESHOLD = {
     '6': 0,
     '7': 0,
 }
-def post_proc(lines):
-    result = []
-    for line in lines:
-        if len(line) == 2:
-            result.append(line[:2])
-            continue
-        fi = line[0]
-        top1 = line[1]
-        score1 = line[2]
-        top2 = line[3]
-        score2 = line[4]
-        img_path = os.path.join(CROP_DIR, fi)
-        if not os.path.exists(img_path):
-            continue
-        print(img_path)
-        img = Image.open(img_path)
-        width, height = img.size
-        if width == height:
-            continue
-        if top1 == '0' and round(width/height, 4) == 1.7778:
-            result.append([fi, top2, score2, top1, score1])
-        elif float(score1) <= 0.5 and top2 == '0':
-            result.append([fi, top2, score2, top1, score1])
-        elif float(score1) <= THRESHOLD[top1]:
-            result.append([fi, top2, score2, top1, score1])
-        else:
-            result.append([fi, top1, score1, top2, score2])
+
+# Yêu cầu file inp_sub phải theo format "file_name  top1    score1  top2    score2"
+def post_proc(INP_SUB_PATH, INP_SUB_FILE, OUT_SUB_PATH, OUT_SUB_FILE, TEST_SET_DIR):
+    fn, _ = os.path.splitext(OUT_SUB_FILE)
+    test_dir = os.path.dirname(TEST_SET_DIR+'/')
+    CROP_DIR = test_dir + '_cropped'
+    NONE_DIR = test_dir + '_none'
+    SQUARE_DIR = test_dir + '_square'
+
+    with open(os.path.join(INP_SUB_PATH, INP_SUB_FILE), 'r') as f:
+        lines = f.read().splitlines()
+
+    lines = [line.strip().split() for line in lines]
+    lines.sort(key=lambda x: x[0])
+    
+    def _post_proc(lines):
+        result = []
+        for line in lines:
+            if len(line) == 2:
+                result.append(line[:2])
+                continue
+            fi = line[0]
+            top1 = line[1]
+            score1 = line[2]
+            top2 = line[3]
+            score2 = line[4]
+            img_path = os.path.join(CROP_DIR, fi)
+            if not os.path.exists(img_path):
+                continue
+            print(img_path)
+            img = Image.open(img_path)
+            width, height = img.size
+            if top1 == '0' and round(width/height, 4) == 1.7778:
+                result.append([fi, top2, score2, top1, score1])
+            elif float(score1) <= 0.5 and top2 == '0':
+                result.append([fi, top2, score2, top1, score1])
+            elif float(score1) <= THRESHOLD[top1]:
+                result.append([fi, top2, score2, top1, score1])
+            else:
+                result.append([fi, top1, score1, top2, score2])
+                
+        return result
             
-    return result
-        
-def populate_none(result):
-    dir = os.listdir(NONE_DIR)
-    for fi in dir:
-        result.append([fi, '0', '1.0000', '0', '1.0000'])
-        
-def populate_square(result):
-    dir = os.listdir(SQUARE_DIR)
-    for fi in dir:
-        result.append([fi, '0', '1.0000', '0', '1.0000'])
+    def _populate_none(result):
+        dir = os.listdir(NONE_DIR)
+        for fi in dir:
+            result.append([fi, '0', '1.0000', '0', '1.0000'])
+            
+    def _populate_square(result):
+        dir = os.listdir(SQUARE_DIR)
+        for fi in dir:
+            result.append([fi, '0', '1.0000', '0', '1.0000'])
 
-result = post_proc(lines)
-populate_none(result)
-populate_square(result)
+    result = _post_proc(lines)
+    
+    _populate_none(result)
+    _populate_square(result)
 
-print(result)
-print(len(result))
+    # print(result)
+    print(len(result))
 
-with open(os.path.join(OUT_SUB_PATH, OUT_SUB_FILE), 'w') as f:
-    for line in result:
-        f.write('\t'.join(line[:2]) + '\n')
+    with open(os.path.join(OUT_SUB_PATH, OUT_SUB_FILE), 'w') as f:
+        for line in result:
+            f.write('\t'.join(line[:2]) + '\n')
 
-with open(os.path.join(OUT_SUB_PATH, "{}_viz.txt".format(fn)), 'w') as f:
-    for line in result:
-        f.write('\t'.join(line) + '\n')
+    with open(os.path.join(OUT_SUB_PATH, "{}_viz.txt".format(fn)), 'w') as f:
+        for line in result:
+            f.write('\t'.join(line) + '\n')
+            
+            
+if __name__ == '__main__':
+    post_proc(INP_SUB_PATH, INP_SUB_FILE, OUT_SUB_PATH, OUT_SUB_FILE, TEST_SET_DIR)
