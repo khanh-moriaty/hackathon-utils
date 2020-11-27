c005214c004323b9affaf3eb83c40c3676f66084
moriaty <19520624@gm.uit.edu.vn>
2020-11-27 14:18:47 +0700

    Updated post_proc

--- post_proc.py
+++ post_proc.py
@@ -3,23 +3,42 @@ import shutil
 from PIL import Image
 
 INP_SUB_PATH = '/storage/timm/'
-INP_SUB_FILE = 'team007_b8tf_datacrop_5epoch.txt'
+INP_SUB_FILE = 'submission_best_tf_efficientnet_b8.txt'
 
 OUT_SUB_PATH = '/storage/submissions/'
-OUT_SUB_FILE = 'team007_b8tf_datacrop_5epoch_threshold0.txt'
+OUT_SUB_FILE = 'team007_b8tf_datacrop_BEST_postproc_final.txt'
 
 TEST_SET_DIR = '/dataset/test_set_A_full/'
 
 THRESHOLD = {
     '0': 0,
-    '1': 0,
+    '1': 0.62,
     '2': 0,
     '3': 0,
-    '4': 0,
+    '4': 0.52,
     '5': 0,
-    '6': 0,
+    '6': 0.41,
     '7': 0,
 }
+
+WIDTH0 = [
+    
+]
+
+HEIGHT0 = [
+    500,
+    640,
+    672,
+    704,
+    736,
+    768,
+    800,
+    832,
+    864,
+    896,
+    928,
+    960,
+]
 
 # Yêu cầu file inp_sub phải theo format "file_name  top1    score1  top2    score2"
 # Trả về path đến 2 file: (submission theo format btc, submission kèm theo score để viz)
@@ -44,19 +63,26 @@ def post_proc(INP_SUB_PATH, INP_SUB_FILE, OUT_SUB_PATH, OUT_SUB_FILE, TEST_SET_D
                 continue
             fi = line[0]
             top1 = line[1]
+            
+            score1 = '1.0'
+            top2 = '7'
+            score2 = '1.0'
+            
             score1 = line[2]
             top2 = line[3]
             score2 = line[4]
+            
             img_path = os.path.join(CROP_DIR, fi)
             if not os.path.exists(img_path):
                 continue
+            img_path = os.path.join(TEST_SET_DIR, fi)
             print(img_path)
             img = Image.open(img_path)
             width, height = img.size
             if top1 == '0' and round(width/height, 4) == 1.7778:
                 result.append([fi, top2, score2, top1, score1])
-            elif float(score1) <= 0.5 and top2 == '0':
-                result.append([fi, top2, score2, top1, score1])
+            elif width in WIDTH0 or height in HEIGHT0:
+                result.append([fi, '0', '1.0', top1, score1])
             elif float(score1) <= THRESHOLD[top1]:
                 result.append([fi, top2, score2, top1, score1])
             else:
@@ -75,6 +101,8 @@ def post_proc(INP_SUB_PATH, INP_SUB_FILE, OUT_SUB_PATH, OUT_SUB_FILE, TEST_SET_D
             result.append([fi, '0', '1.0000', '0', '1.0000'])
 
     result = _post_proc(lines)
+    
+    print(len(result))
     
     _populate_none(result)
     _populate_square(result)
--- viz.py
+++ viz.py
@@ -4,13 +4,39 @@ from PIL import Image
 
 INP_DIR = '/dataset/test_set_A_full/'
 OUT_DIR = '/dataset/viz/'
-SUBMISSION_NAME = 'team007_b8tf_datacrop_5epoch_threshold0_viz.txt'
+SUBMISSION_NAME = 'team007_b8tf_datacrop_BEST_postproc_final_viz.txt'
 MAX_SIZE = 200
 
 SUBMISSION_PATH = '/storage/submissions/'
 submission_path = os.path.join(SUBMISSION_PATH, SUBMISSION_NAME)
 lines = open(submission_path, 'r').read().splitlines()
 lines.sort()
+
+SPECIAL_SIZE = [
+    (1000, 677),
+    (1024, 683),
+    (960, 720),
+    (1080, 720),
+    (1000, 750),
+    (1912, 1080),
+    (512, 288),
+    (426, 240),
+    (500, 300),
+    (459, 306),
+    (500, 313),
+    (640, 360),
+    (660, 371),
+    (600, 400),
+    (660, 410),
+    (640, 427),
+    (660, 440),
+    (665, 449),
+    (500, 450),
+    (800, 450),
+    (656, 480),
+    (653, 490),
+    (660, 495),
+]
 
 submission_name, _ = os.path.splitext(SUBMISSION_NAME)
 OUT_DIR = os.path.join(OUT_DIR, submission_name)
@@ -25,6 +51,8 @@ all_img = os.listdir(INP_DIR)
 pred_img = []
 
 # fo = open(os.path.join(SUBMISSION_PATH, submission_name + '_viz.txt'), 'w')
+
+viz_count = 0
 
 for line in lines[:]:
     line = line.split()
@@ -43,26 +71,27 @@ for line in lines[:]:
     # if abs(width/height - 16/9) < 0.0001 and name == '0': 
     #     name = '2'
     #     print('16:9', inp_path)
-        
-        
-    if len(line) > 4:
-        score = float(line[2])
-        fn, ext = os.path.splitext(out_name)
-        out_path = os.path.join(OUT_DIR, name, "{:.4f}_{}_{:.4f}_{}{}".format(score, line[3], float(line[4]), fn, ext))
-    elif len(line) > 2:
-        score = float(line[2])
-        fn, ext = os.path.splitext(out_name)
-        out_path = os.path.join(OUT_DIR, name, "{:.4f}_{}{}".format(score, fn, ext))
-    else:
-        out_path = os.path.join(OUT_DIR, name, out_name)
-    
-    # fo.write("{}\t{}\n".format(out_name, name))
     
     
     img = cv2.imread(inp_path)
     if img is None: continue
     height, width = img.shape[:2]
     if width == height: continue
+        
+    special = 'SPECIAL' if (width, height) in SPECIAL_SIZE else 'XXXXXXX'
+        
+    if len(line) > 4:
+        score = float(line[2])
+        fn, ext = os.path.splitext(out_name)
+        out_path = os.path.join(OUT_DIR, name, "{}_{:.4f}_{}_{:.4f}_{}{}".format(special, score, line[3], float(line[4]), fn, ext))
+    elif len(line) > 2:
+        score = float(line[2])
+        fn, ext = os.path.splitext(out_name)
+        out_path = os.path.join(OUT_DIR, name, "{}_{:.4f}_{}{}".format(special, score, fn, ext))
+    else:
+        out_path = os.path.join(OUT_DIR, name, out_name)
+    
+    # fo.write("{}\t{}\n".format(out_name, name))
     img = cv2.resize(img, (width * MAX_SIZE // height, MAX_SIZE))
     cv2.imwrite(out_path, img)
     
